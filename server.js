955fce53c87aa182495db722ea2e9b17432ad096
pmc-muchiri <pmc.ac.ke@gmail.com>
2025-09-20 12:02:13 +0300

    removed coments from server.js

--- server.js
+++ server.js
@@ -1,40 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('./_config');
 
-// Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
+const index = require('./routes/index');
+const image = require('./routes/image');
 
-// Initializing the app
 const app = express();
-
-// connecting the database
-// let mongodb_url = 'mongodb://localhost:27017/';
-// let dbName = 'darkroom';
-// // mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-// //     if (err) console.log(err)
-// // });
-// const env = process.env.NODE_ENV || 'development';
-// const dbURI = config.mongoURI[env];
-
-// console.log(`NODE_ENV = ${env}`);
-// console.log(`Connecting to: ${dbURI}`);
-
-// mongoose.connect(dbURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log(`Database connected successfully [${env}]`))
-// .catch(err => console.error('MongoDB connection error:', err));
-
-// // test if the database has connected successfully
-// // let db = mongoose.connection;
-// // db.once('open', ()=>{
-// //     console.log('Database connected successfully')
-// // })
 
 // Build MongoDB URI from .env
 const {
@@ -46,33 +18,27 @@ const {
 
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB_DEV}?retryWrites=true&w=majority`;
 
-
-// Connect to MongoDB
+// Connect to MongoDB (Mongoose 6+ defaults are used; no warnings)
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Database connected successfully!!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
+// Body parser middleware
+app.use(express.json());
 
-
+// Routes
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
-
 
 module.exports = app;
