a6315663cc9e94e56b29aab9870ec0352cf960f2
pmc-muchiri <pmc.ac.ke@gmail.com>
2025-09-20 08:49:53 +0300

    updated server to connect to mongoDB atlas

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./_config');
+require('dotenv').config();
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// Initializing the app
-const app = express();
-
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
+// let mongodb_url = 'mongodb://localhost:27017/';
+// let dbName = 'darkroom';
 // mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
 //     if (err) console.log(err)
 // });
-const env = process.env.NODE_ENV || 'development';
-const dbURI = config.mongoURI[env];
 
-console.log(`NODE_ENV = ${env}`);
-console.log(`Connecting to: ${dbURI}`);
+// Build MongoDB URI from .env
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_CLUSTER,
+  MONGO_DB_DEV
+} = process.env;
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log(`Database connected successfully [${env}]`))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB_DEV}?retryWrites=true&w=majority`;
 
 
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Initializing the app
+const app = express();
 
 
 // View Engine
@@ -59,6 +55,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
-
-
-module.exports = app;
