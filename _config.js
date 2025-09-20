5bbe20563a4953625e59138db2e88f0713df1b19
pmc-muchiri <pmc.ac.ke@gmail.com>
2025-09-20 08:31:37 +0300

    updated darroom variables

--- _config.js
+++ _config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 var config = {};
 
-const { MONGO_USERNAME: username, MONGO_PASSWORD: password, MONGO_CLUSTER: cluster, APP_VALUE: app_value} = process.env;
+const { MONGO_USERNAME: username, MONGO_PASSWORD: password, MONGO_CLUSTER: cluster, APP_VALUE: app_value,MONGO_DB: prod, MONGO_DB_DEV: dev, MONGO_DB_TEST:test} = process.env;
 
 config.mongoURI = {
-    production: `mongodb+srv://${username}:${password}@${cluster}/darkroom?retryWrites=true&w=majority&appName=${app_value}`,
-    development: `mongodb+srv://${username}:${password}@${cluster}/darkroom-dev?retryWrites=true&w=majority&appName=${app_value}`,
-    test: `mongodb+srv://${username}:${password}@${cluster}/darkroom-test?retryWrites=true&w=majority&appName=${app_value}`,
+    production: `mongodb+srv://${username}:${password}@${cluster}/${prod}?retryWrites=true&w=majority&appName=${app_value}`,
+    development: `mongodb+srv://${username}:${password}@${cluster}/${dev}?retryWrites=true&w=majority&appName=${app_value}`,
+    test: `mongodb+srv://${username}:${password}@${cluster}/${test}?retryWrites=true&w=majority&appName=${app_value}`,
 
     }
 module.exports = config;
